2e520d2d686ec515bc75f1ae32e96a2499b72dd8
Lalit Puri <laleetpuree5768@gmail.com>
2022-04-18 17:08:37 +0530

    some more bug fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,8 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="bg-[#26331A]">
-      <div className="md:mx-32 mx-10 flex flex-col md:flex-row justify-evenly items-center py-6">
-        <ul className="flex text-lg font-semibold gap-10 text-[#98B66E] text-l mr-0 items-center">
-          <Link to="/terms">
-            <li className="link">Terms</li>
-          </Link>
-          <Link to="/privacy">
-            <li className="link">Privacy Policy</li>
-          </Link>
-        </ul>
+      <div className="md:mx-32 mx-10 flex flex-col md:flex-row justify-center items-center py-6">
+        {/*  */}
         <p>
           <p className="text-lg flex items-center gap-2 text-[#98B66E] cursor-pointer">
             <span className="text-2xl mt-1">&copy;</span>Copyright 2022 Imali
